390ba54e3646af69ca496e02feb891bf46bf1eda
Andrew-Romanov <andrewromanov@yahoo.com>
2021-04-13 18:42:38 +0300

    Добавлены варианты закрытия модалки

--- js/gallery.js
+++ js/gallery.js
@@ -2,43 +2,55 @@ import galleryItems from "../gallery-items.js";
 
 const galleryElement = document.querySelector('.js-gallery');
 const modalElement = document.querySelector('.js-lightbox');
+const modalOverlayElement = document.querySelector('.lightbox__overlay');
 const modalCloseButtonElement = document.querySelector('.lightbox__button[data-action="close-lightbox"]');
 const modalImageElement = document.querySelector('.lightbox__image');
 
-function galleryClickHandler (event) {
+function modalOpen(event) {
+  event.preventDefault();
   if (event.target.nodeName !== 'IMG') return;
   modalImageElement.src = galleryItems[Number(event.target.dataset.value)].original;
   modalImageElement.alt = galleryItems[Number(event.target.dataset.value)].description;
   modalElement.classList.add('is-open');
+  window.addEventListener('keydown', modalCloseOnEscapeKey);
 };
 
-function modalCloseButtonClickHandler (event) {
+function modalClose() {
   modalElement.classList.remove('is-open');
   modalImageElement.alt = '';
   modalImageElement.src = '';
+  window.removeEventListener('keydown', modalCloseOnEscapeKey);
 };
 
-// console.log(
-//   galleryItems.map((element) => {
-//     return `<li class="gallery__item"><img class="gallery__image" src="${element.preview}" alt="${element.description}"></li>`;
-//   }).join("")
-// );
+function modalCloseOnEscapeKey(event) {
+  if (event.code === 'Escape') modalClose();
+};
 
 const galleryElementMarkup = galleryItems.map((element, index) => {
-  return `<li class="gallery__item"><img class="gallery__image" data-value="${index}" src="${element.preview}" alt="${element.description}"></li>`;
+  return `<li class="gallery__item">
+    <a
+      class="gallery__link"
+      href="${element.original}">
+      <img
+        class="gallery__image"
+        data-value="${index}"
+        data-source="${element.original}"
+        src="${element.preview}"
+        alt="${element.description}">
+    </a>
+    </li>`;
   }).join("");
 
 galleryElement.insertAdjacentHTML('afterbegin', galleryElementMarkup);
 
 galleryElement.addEventListener('click', (event) => {
-  galleryClickHandler(event);
+  modalOpen(event);
 });
 
-modalCloseButtonElement.addEventListener('click', (event) => {
-  modalCloseButtonClickHandler(event);
+modalCloseButtonElement.addEventListener('click', () => {
+  modalClose();
 });
 
-// for (let i = 1; i <= 9; i++) {
-//   galleryElement.insertAdjacentHTML('afterbegin',
-//     `<li class="gallery__item"><img class="gallery__image" src="${galleryItems[0].preview}" alt="${galleryItems[0].description}"></li>`);
-// };
+modalOverlayElement.addEventListener('click', (event) => {
+  if (event.target === event.currentTarget) modalClose();
+});
